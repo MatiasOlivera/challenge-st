10541c0807e9b948179d4ca6f1142144ed3cefa2
Matías Olivera <matiasolivera@outlook.com>
2025-08-15 16:10:21 -0300

    feat: add Next boilerplate

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+export const metadata = {
+    title: 'React & Next.js Updates',
+    description: 'React 18 and Next.js 15 features',
+  }
+  
+  export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    )
+  }
+  
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Next.js!</h1>
+      <p>This is your default App page.</p>
+    </main>
+  );
+}
