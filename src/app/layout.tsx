bcbd6b76b1fc29c97b0bd00416e76214bfe4e483
Matías Olivera <matiasolivera@outlook.com>
2025-08-28 15:46:03 -0300

    feat: fix linter errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,10 @@ import type { Metadata, Viewport } from "next";
 import {
   NavigationMenu,
   NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
 
 export const metadata: Metadata = {
