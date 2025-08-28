a55b332a4a75d75d35f15cb2ad89db3a9296dff3
Matías Olivera <matiasolivera@outlook.com>
2025-08-28 15:08:21 -0300

    feat: add debounce to search

--- src/app/phrases/hooks/useSearch.ts
+++ src/app/phrases/hooks/useSearch.ts
@@ -1,13 +1,18 @@
 import React, { useState, useDeferredValue } from "react";
 import type { OnSearchChange } from "../types/phrases.d.ts";
+import { useDebounce } from "@/app/hooks/useDebounce";
 
 export function useSearch(onSearchChange: OnSearchChange): UseSearchHook {
     const [searchTerm, setSearchTerm] = useState("");
     const deferredSearchTerm = useDeferredValue(searchTerm);
+    const debouncedSearchTerm = useDebounce(deferredSearchTerm, 300);
 
     React.useEffect(() => {
-        onSearchChange(deferredSearchTerm);
-    }, [deferredSearchTerm, onSearchChange]);
+        if (debouncedSearchTerm.length === 0 || debouncedSearchTerm.length >= 3) {
+            onSearchChange(debouncedSearchTerm);
+        }
+
+    }, [debouncedSearchTerm, onSearchChange]);
 
     const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchTerm(e.target.value);
